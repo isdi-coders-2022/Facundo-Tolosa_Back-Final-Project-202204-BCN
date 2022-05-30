8e540dbe7c8f75abe91b6e06f90cfd0b7a13c252
Facundo Tolosa <facundotolosa98@gmail.com>
2022-05-30 18:13:16 +0200

    Connected to the database

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 require("dotenv").config();
-
+const debug = require("debug")("amazingN:root");
+const chalk = require("chalk");
+const connectDB = require("./database");
 const initializeServer = require("./server/initializeServer");
 
-const port = process.env.PORT ?? 4000;
+const port = process.env.SERVER_PORT || 3000;
+const mongoConnection = process.env.MONGODB_STRING;
 
-initializeServer(port);
+(async () => {
+  try {
+    await connectDB(mongoConnection);
+    await initializeServer(port);
+  } catch (error) {
+    debug(chalk.red(`Error: `, error.message));
+  }
+})();
